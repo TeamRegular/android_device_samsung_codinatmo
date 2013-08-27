803a188ad0a468ad12089370f13e16d954f5c86d
C457 <android.c357@gmail.com>
2013-08-27 00:40:52 -0500

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/samsung/u8500-common/BoardCommonConfig.mk
+TARGET_OTA_ASSERT_DEVICE := codinatmo,t599,SGH-T599
+
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := DB8520H
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/codinatmo/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/codinatmo/bluetooth/btvendor_codinatmo.txt
+
+# Wifi
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin_b2"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin_b2"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin_b2"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
+WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
+
+# Partitions
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+#BOARD_FLASH_BLOCK_SIZE := 131072
+#TARGET_USERIMAGES_USE_EXT4 := true
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 5064622080
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_codinatmo_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/codinatmo
+TARGET_USE_ST_ERICSSON_KERNEL := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 25
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/samsung/codinatmo/recovery/recovery.rc
---
+++ codinatmo.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit the proprietary vendors blobs for Samsung Codinatmo.
+$(call inherit-product-if-exists, vendor/samsung/codinatmo/codinatmo-vendor.mk)
+
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit common configuration for Samsung u8500 devices
+$(call inherit-product-if-exists, device/samsung/u8500-common/device_u8500-common.mk)
+
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.samsungcodina:root/fstab.samsungcodina \
+    $(LOCAL_PATH)/rootdir/init.samsungcodina.rc:root/init.samsungcodina.rc \
+    $(LOCAL_PATH)/rootdir/init.samsungcodina.usb.rc:root/init.samsungcodina.usb.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc
+
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/AT/manuf_id.cfg:system/etc/AT/manuf_id.cfg \
+    $(LOCAL_PATH)/configs/etc/AT/model_id.cfg:system/etc/AT/model_id.cfg \
+    $(LOCAL_PATH)/configs/etc/AT/system_id.cfg:system/etc/AT/system_id.cfg
+
+# TouchScreen & Inputs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/usr/keylayout/codina-kp.kl:system/usr/keylayout/codina-kp.kl
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    ste.cam.ext.cfg.path.secondary=/system/usr/share/camera/config_file/aptina_mt9v113.dat
+
+# Vold
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/vold.fstab:system/etc/vold.fstab
